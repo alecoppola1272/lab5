0daf32a486ef1028d7eecfb04c6dc1046c99e243
Alessandro Coppola <alessandro.coppola5@studio.unibo.it>
2022-05-02 18:25:40 +0200

    Minor changes

--- chain.hpp
+++ chain.hpp
@@ -1,6 +1,7 @@
 #ifndef CHAIN_HPP
 #define CHAIN_HPP
 #include <cmath>
+#include <stdexcept>
 #include <vector>
 
 struct PPState {
@@ -11,21 +12,30 @@ struct PPState {
 
 class Hooke {
   double m_k;
-  double l_k;
+  double m_l;
 
  public:
-  Hooke(double k, double l) : m_k{k}, l_k{l} {};
-  double operator()(PPState const& p1, PPState const& p2) const {
-    double f;
-    if (p2.x - p1.x == 0) {
-      return f = 0;
+  Hooke(double k, double l) : m_k{k}, m_l{l} {
+    if (m_k <= 0.) {
+      throw std::runtime_error{"Hookes's parameter k must be > 0"};
     }
-    f = (abs(p2.x - p1.x) - l_k) * m_k;
-    return f;
+
+    if (m_l < 0) {
+      throw std::runtime_error{"Hooke's parameter l must be =>0 "};
+    }
+  };
+
+  double operator()(PPState const& p1, PPState const& p2) const {
+    double F;
+    if (p2.x - p1.x == 0) {
+      return F = 0;
+    }
+    F = (abs(p2.x - p1.x) - m_l) * m_k;
+    return F;
   }
 };
 
-//From Now Not Tested
+// From Now Not Tested
 class Chain {
   Hooke m_inter;
   std::vector<PPState> m_ppses;
--- chain_test.cpp
+++ chain_test.cpp
@@ -4,36 +4,42 @@
 #include "doctest.h"
 
 TEST_CASE("Testing Hooke") {
-  Hooke hooke{2., 10.};
+  SUBCASE("Hooke's parameter k<0") { CHECK_THROWS(Hooke{-1., 1.}); }
 
-  SUBCASE("Attractive Force") {
+  SUBCASE("Hooke's parameter k=0") { CHECK_THROWS(Hooke{0., 1.}); }
+
+  SUBCASE("Hooke's parameter l<0") { CHECK_THROWS(Hooke{2., -1.}); }
+
+  SUBCASE("Attractive Force F>0") {
+    Hooke hooke{2., 10.};
     PPState p1{1., 0., 0.};
     PPState p2{1., 12., 0.};
-    double f = hooke(p1, p2);
-    CHECK(f == doctest::Approx(4.));
+    CHECK(hooke(p1, p2) == doctest::Approx(4.));
   }
-  SUBCASE("Inverted Points") {
+  SUBCASE("Inverted Points (F>0)") {
+    Hooke hooke{2., 10.};
     PPState p1{1., 0., 0.};
     PPState p2{1., 12., 0.};
-    double f = hooke(p2, p1);
-    CHECK(f == doctest::Approx(4.));
+    CHECK(hooke(p2, p1) == doctest::Approx(4.));
   }
-  SUBCASE("Spring at Rest") {
+  SUBCASE("Spring at Rest F=0") {
+    Hooke hooke{2., 10.};
     PPState p1{1., 0., 0.};
     PPState p2{1., 10., 0.};
-    double f = hooke(p1, p2);
-    CHECK(f == (0.));
+    CHECK(hooke(p1, p2) == (0.));
   }
-  SUBCASE("Repulsive Force") {
+  SUBCASE("Repulsive Force F<0") {
+    Hooke hooke{2., 10.};
     PPState p1{1., 0., 0.};
     PPState p2{1., 8., 0.};
-    double f = hooke(p1, p2);
-    CHECK(f == doctest::Approx(-4.));
+    CHECK(hooke(p1, p2) == doctest::Approx(-4.));
   }
 
   SUBCASE("Force on the same point") {
+    Hooke hooke{2., 10.};
     PPState p1{1., 10., 0.};
-    double f = hooke(p1, p1);
-    CHECK(f == (0.));
+    PPState p2{1., 10., 1.};
+    CHECK(hooke(p1, p1) == (0.));
+    CHECK(hooke(p1, p2) == (0.));
   }
 }
